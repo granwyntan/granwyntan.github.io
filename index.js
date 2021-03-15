52112f8ffb65e2dcc50f534e8a5646c4e84dfb5a
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2021-03-15 16:02:12 +0800

    Improvements

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = "Today's date: "+formatDate;
+
+function myFunction() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
