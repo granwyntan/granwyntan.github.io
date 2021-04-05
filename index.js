0e38b78fa43ddffd5d6c6a3b9c702f01cd71c46f
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2021-04-05 23:54:25 +0800

    Many more enhancements to current existing website

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = "Today's date: "+formatDate;
 
-function myFunction() {
+function myOtherFunction() {
     var x = document.getElementById("Yes");
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
@@ -27,3 +27,36 @@ function myFunction() {
       x.className = x.className.replace(" w3-show", "");
     }
   }
+function myFunction(x) {
+  x.classList.toggle("change");
+  theFunction();
+}
+
+function theFunction() {
+  var x = document.getElementById("myTopnav");
+  // var y = document.getElementById("menuIcon");
+  if (x.className === "w3-bar w3-black w3-card w3-top topnav") {
+    x.className += " responsive";
+    x.style = "position:fixed;top:0;"
+    // y.style.transform = "translateY(-12px) rotate(-135deg);"
+  } else {
+    x.className = "w3-bar w3-black w3-card w3-top topnav";
+    x.style = "position:fixed;top:0; display:flex; align-items:center; overflow:hidden;"
+    // y.style.transform = "translateY(-12px) rotate(-135deg);"
+  }
+  // const menuBtn = document.querySelector('.nav-icon');
+  // let menuOpen = false;
+  // menuBtn.addEventListener('click', () => {
+  //   if(!menuOpen) {
+  //     menuBtn.classList.add('open');
+  //     menuOpen = true;
+  //   } else {
+  //     menuBtn.classList.remove('open');
+  //     menuOpen = false;
+  //   }
+  // });
+  function toggleMenu() {
+    y = document.querySelector(".header__hamburger");
+    y.classList.toggle("click");
+  }
+}
